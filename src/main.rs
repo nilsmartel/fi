d930b42154f233109584f1c77700ec792fe0c9c9
Nils Martel <nils.martel@graphmasters.net>
2020-01-27 20:59:21 +0100

    Clean up a little

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod state;
 
 use std::io::{stdin, stdout, Write};
-use termion::event::{Event, Key, MouseEvent};
-use termion::input::{MouseTerminal, TermRead};
-use termion::raw::IntoRawMode;
+use termion::{
+    event::{Event, Key, MouseEvent},
+    input::{MouseTerminal, TermRead},
+    raw::IntoRawMode,
+};
+use tui::{backend::TermionBackend, terminal::Terminal};
 
 fn main() {
     let stdin = stdin();
-    let term = Terminal::new();
+    let mut term = terminal();
+    term.clear();
 
     for c in stdin.events() {
         let evt = c.unwrap();
@@ -15,12 +19,19 @@ fn main() {
             Event::Key(Key::Char('q')) => break,
             Event::Mouse(me) => match me {
                 MouseEvent::Press(_, x, y) => {
-                    write!(stdout, "{}x", termion::cursor::Goto(x, y)).unwrap();
+                    // write!(stdout, "{}x", termion::cursor::Goto(x, y)).unwrap();
                 }
                 _ => (),
             },
             _ => {}
         }
-        stdout.flush().unwrap();
+        term.flush().unwrap();
     }
+}
+
+fn terminal() -> Terminal<impl tui::backend::Backend> {
+    let stdout = stdout().into_raw_mode().unwrap();
+    let stdout = MouseTerminal::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    Terminal::new(backend).unwrap()
 }
