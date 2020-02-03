6d9648f47f84a97aaf142aab1a0eb7939e000bd5
Nils Martel <nils.martel@graphmasters.net>
2020-02-03 17:36:35 +0100

    Include UI module

--- src/state.rs
+++ src/state.rs
@@ -1,16 +1,15 @@
 use std::collections::HashSet;
 
 use crate::event::{Event, Key, MouseEvent};
-
-type Ui = ();
+use crate::Ui;
 
 pub struct State {
     // Ui Layers
-    pub ui: Vec<Ui>,
+    pub ui: Vec<Box<Ui>>,
 }
 
 pub trait StateMachine {
     fn state(&self) -> &State;
-    fn next(self, event: Event) -> dyn StateMachine;
+    fn next(self, event: Event) -> Box<dyn StateMachine>;
     fn hint_input(&self) -> HashSet<Key>;
 }
