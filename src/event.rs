949bbbce006efb3144a269fef9783b5b5f9c21de
Nils Martel <nils.martel@graphmasters.net>
2020-02-03 17:14:56 +0100

    Introduce Signal Event sending raw bytes

--- src/event.rs
+++ src/event.rs
@@ -4,5 +4,11 @@ pub enum Event {
     Key(Key),
     Mouse(MouseEvent),
     Time(std::time::SystemTime),
-    Signal,
+    SignalRaw(Vec<u8>),
+}
+
+impl From<char> for Event {
+    fn from(c: char) -> Event {
+        let key = Key::Char(
+    }
 }
