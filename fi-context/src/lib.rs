f0ee1f207b35490c1be3cd96dd9f91c354eb961f
Nils Martel <nilsmartel@yahoo.de>
2022-03-25 14:28:44 +0100

    Improve handler code

--- fi-context/src/lib.rs
+++ fi-context/src/lib.rs
@@ -5,30 +5,63 @@ pub type Event = winit::event::Event<'static, ()>;
 
 use std::any::{type_name, Any};
 pub use std::ops::Range;
+use std::sync::Mutex;
 use std::sync::RwLock as Rw;
 
-pub type Context = Rw<EditorContext>;
+type InputHandler = Mutex<Box<dyn Fn(&Context, Event, &dyn Any)>>;
 
-type InputHandler = Box<dyn Fn(&Context, Event, &dyn Any)>;
+#[cfg(test)]
+mod context_tests {
+    use super::*;
 
-pub struct EditorContext {
+    #[test]
+    fn input_state() {
+        let mut ctx = Context::default();
+
+        ctx.set_handler(
+            Mutex::new(String::from("hello world")),
+            |ctx, event, state| {
+                let mut state = state.lock().unwrap();
+                state.push_str(", this is from the handler");
+
+                assert_eq!("hello world, this is from the handler", *state);
+            },
+        );
+    }
+}
+
+pub struct Context {
     pub buffers: Rw<Vec<Buffer>>,
     pub layout: Rw<Layout>,
     pub redraw_requested: Rw<bool>,
 
     input_handler: InputHandler,
-    handler_state: Box<dyn Any>,
+    /// Runtime checked editable object
+    handler_state: Rw<Box<dyn Any>>,
 }
 
-impl EditorContext {
-    pub fn set_handler<T: Sized + 'static>(
-        &mut self,
-        f: impl Fn(&Context, Event, &T) + 'static,
-        state: T,
-    ) {
-        self.handler_state = Box::new(state) as Box<dyn Any>;
+impl std::default::Default for Context {
+    fn default() -> Self {
+        Self {
+            buffers: Default::default(),
+            layout: Default::default(),
+            redraw_requested: Default::default(),
+            input_handler: Mutex::new(Box::new(|_, _, _| {})),
+            handler_state: Rw::new(Box::new(false)),
+        }
+    }
+}
 
-        self.input_handler = Box::new(move |ctx, event, state| {
+impl Context {
+    pub fn set_handler<T: Sized + 'static>(
+        &self,
+        state: T,
+        f: impl Fn(&Context, Event, &T) + 'static,
+    ) {
+        *self.handler_state.write().expect("to set state of handler") =
+            Box::new(state) as Box<dyn Any>;
+
+        *self.input_handler.lock().expect("to set handler") = Box::new(move |ctx, event, state| {
             f(
                 ctx,
                 event,
@@ -48,6 +81,7 @@ pub struct Buffer {
 }
 
 /// Layout and editing information
+#[derive(Default)]
 pub struct Layout {
     /// currently open and edited tab
     pub active_tab: usize,
@@ -57,6 +91,7 @@ pub struct Layout {
 }
 
 /// a Tab may consist of multiple open splits
+#[derive(Default)]
 pub struct Tab {
     /// current split inside the tab, that is to be edited
     pub active_split: usize,
@@ -67,6 +102,7 @@ pub struct Tab {
 
 /// A split is a view into an Buffer
 /// It's a necessity in order for a user to edit the buffers
+#[derive(Default)]
 pub struct Split {
     /// ID of the Buffer this split is a view into.
     /// The original Buffer lies in the [Context]
@@ -75,6 +111,7 @@ pub struct Split {
     pub selected: Vec<Range<usize>>,
 }
 
+#[derive(Default)]
 pub struct Position {
     pub row: usize,
     pub col: usize,
